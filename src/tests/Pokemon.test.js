09aa605b57b2f2704c24cc5686d6f20f52ee2397
Mixchelle <michelle.biologia@hotmail.com>
2022-11-23 12:04:21 -0300

    Iniciando requiaito 7

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+
+describe('teste componente <Pokemon />', () => {
+test('', () => {
+  renderWithRouter(<App />);
+
+test('', () => {
+  renderWithRouter(<App />);
+});
+
+});
